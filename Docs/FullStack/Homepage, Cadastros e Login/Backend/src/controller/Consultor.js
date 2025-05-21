1f20f5809b5270eb143828784c98418f49dace15
Gabriel-Alves02 <gabrielalvescoelho947@gmail.com>
2025-05-21 16:15:36 -0300

    Adicionando consulta da média de avaliação do consultor no controller Consultor.js

--- Docs/FullStack/Homepage, Cadastros e Login/Backend/src/controller/Consultor.js
+++ Docs/FullStack/Homepage, Cadastros e Login/Backend/src/controller/Consultor.js
@@ -104,3 +104,40 @@ export const GetHabilidades = async (request, response, next) => {
 
     }
 };
+
+export const ConsultarMediaConsultor = async (request, response, next) => {
+
+  try {
+
+    const { id } = request.params;
+
+    const [mediaConsultor] = await pool.query
+        (
+          `SELECT AVG(avaliacao) FROM Reuniao
+            GROUP BY idConsultor
+            HAVING idConsultor = ?;
+        `, [id]
+        );
+
+      if (mediaConsultor) {
+        return response.status(200).json
+          ({
+            success: true,
+            mediaConsultor
+          });
+      }
+
+
+    return response.status(201).json({
+      success: true,
+      message: "Media não encontrada."
+    });
+
+  } catch (error) {
+    console.error('Erro no cadastro:', error);
+    return response.status(500).json({
+      success: false,
+      message: "Erro interno do servidor"
+    });
+  }
+};
